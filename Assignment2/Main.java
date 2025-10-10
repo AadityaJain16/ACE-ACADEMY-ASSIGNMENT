e1dd0a07ac418ccade32e8c948fa8700b8a1a359
AadityaJain16 <aadityajain1611@gmail.com>
2025-10-10 15:02:21 +0530

    Changes after output checking
    makes changes in the code after reviewing the assignment 1

--- Assignment2/Main.java
+++ Assignment2/Main.java
@@ -22,77 +22,85 @@ public class Main {
             System.out.println("11. Exit");
 
             System.out.print("Enter your choice: ");
-            option = scannerobj.nextInt();
-            scannerobj.nextLine(); // consume leftover newline
+            if(scannerobj.hasNextInt()) {
+                option = scannerobj.nextInt();
+                scannerobj.nextLine(); // consume leftover newline
 
-            switch (option) {
-                case 1:
-                    System.out.println("Enter a string to count unique palindromes:");
-                    String PallindromeString = scannerobj.nextLine();
-                    System.out.println(stringOperatorobj.countUniquePalindromes(PallindromeString));
-                    break;
+                switch (option) {
+                    case 1:
+                        System.out.println("Enter a string to count unique palindromes:");
+                        String PallindromeString = scannerobj.nextLine();
+                        System.out.println(stringOperatorobj.countUniquePalindromes(PallindromeString));
+                        break;
 
-                case 2:
-                    System.out.println("Enter the term number:");
-                    int term = scannerobj.nextInt();
-                    System.out.println(stringOperatorobj.FibonacciNumberTerm(term));
-                    break;
+                    case 2:
+                        System.out.println("Enter the term number:");
+                        int term = scannerobj.nextInt();
+                        if(term<0)
+                            System.out.println("Enter valid term ie term should be grreter eaqual to 0");
+                        System.out.println(stringOperatorobj.FibonacciNumberTerm(term));
+                        break;
 
-                case 3:
-                    System.out.println("Enter string in Snake case to convert into Camel case:");
-                    String snakeString = scannerobj.nextLine();
-                    System.out.println(stringOperatorobj.SnakeToCamel(snakeString));
-                    break;
+                    case 3:
+                        System.out.println("Enter string in Snake case to convert into Camel case:");
+                        String snakeString = scannerobj.nextLine();
+                        System.out.println(stringOperatorobj.SnakeToCamel(snakeString));
+                        break;
 
-                case 4:
-                    System.out.println("Enter string to count consonants:");
-                    String consStr = scannerobj.nextLine();
-                    System.out.println(stringOperatorobj.CountConsonant(consStr));
-                    break;
+                    case 4:
+                        System.out.println("Enter string to count consonants:");
+                        String consStr = scannerobj.nextLine();
+                        System.out.println(stringOperatorobj.CountConsonant(consStr));
+                        break;
 
-                case 5:
-                    System.out.println("Enter number in binary to convert into decimal:");
-                    String binaryNumber = scannerobj.nextLine();
-                    System.out.println(stringOperatorobj.BinaryToDecimalConvertor(binaryNumber));
-                    break;
+                    case 5:
+                        System.out.println("Enter number in binary to convert into decimal:");
+                        String binaryNumber = scannerobj.nextLine();
+                        System.out.println(stringOperatorobj.BinaryToDecimalConvertor(binaryNumber));
+                        break;
 
-                case 6:
-                    System.out.println("Enter the string to expand:");
-                    String compressedString = scannerobj.nextLine();
-                    System.out.println(stringOperatorobj.ExpandString(compressedString));
-                    break;
+                    case 6:
+                        System.out.println("Enter the string to expand:");
+                        String compressedString = scannerobj.nextLine();
+                        System.out.println(stringOperatorobj.ExpandString(compressedString));
+                        break;
 
-                case 7:
-                    System.out.println("Enter String to compress:");
-                    String expandedString = scannerobj.nextLine();
-                    System.out.println(stringOperatorobj.CompressString(expandedString));
-                    break;
+                    case 7:
+                        System.out.println("Enter String to compress:");
+                        String expandedString = scannerobj.nextLine();
+                        System.out.println(stringOperatorobj.CompressString(expandedString));
+                        break;
 
-                case 8:
-                    System.out.println("Enter number to check whether it is prime or not:");
-                    int number = scannerobj.nextInt();
-                    System.out.println(stringOperatorobj.PrimeNumberChecker(number));
-                    break;
+                    case 8:
+                        System.out.println("Enter number to check whether it is prime or not:");
+                        int number = scannerobj.nextInt();
+                        System.out.println(stringOperatorobj.PrimeNumberChecker(number));
+                        break;
 
-                case 9:
-                    System.out.println("Enter number to convert to words:");
-                    int numToWord = scannerobj.nextInt();
-                    System.out.println(stringOperatorobj.NumberToWords(numToWord));
-                    break;
+                    case 9:
+                        System.out.println("Enter number to convert to words:");
+                        int numToWord = scannerobj.nextInt();
+                        System.out.println(stringOperatorobj.NumberToWords(numToWord));
+                        break;
 
-                case 10:
-                    System.out.println("Enter string to find longest substring without repeating characters:");
-                    String longSub = scannerobj.nextLine();
-                    System.out.println(stringOperatorobj.LongestUniqueSubstring(longSub));
-                    break;
+                    case 10:
+                        System.out.println("Enter string to find longest substring without repeating characters:");
+                        String longSub = scannerobj.nextLine();
+                        System.out.println(stringOperatorobj.LongestUniqueSubstring(longSub));
+                        break;
 
-                case 11:
-                    System.out.println("Exiting program...");
-                    exit = true;
-                    break;
+                    case 11:
+                        System.out.println("Exiting program...");
+                        exit = true;
+                        break;
 
-                default:
-                    System.out.println("Enter correct choice!");
+                    default:
+                        System.out.println("Enter correct choice!");
+                }
+            }
+            else {
+                System.out.println("Enter valid Choice!!!!!");
+                exit=true;
             }
         }
 
--- Assignment2/StringOperator.java
+++ Assignment2/StringOperator.java
@@ -11,6 +11,10 @@ public class StringOperator {
     }
     public String SnakeToCamel(String Snake_String)
     {
+        if(Snake_String=="")
+        {
+            return "Enter valid string";
+        }
         String cammelString="";
         boolean nextUpperCase=false;
         for(int i=0;i<Snake_String.length();i++)
@@ -46,13 +50,18 @@ public class StringOperator {
         }
         return count;
     }
-    public int BinaryToDecimalConvertor(String BinaryNumber)
+    public long BinaryToDecimalConvertor(String BinaryNumber)
     {
-        int decimal=0;
+        long  decimal=0L;
         int power=0;
         for(int i=BinaryNumber.length()-1;i>=0;i--)
         {
             char bit=BinaryNumber.charAt(i);
+            if(bit!='1'&&bit!='0')
+            {
+                System.out.println("Enter valid binary number!!!");
+                return 0;
+            }
             if(bit=='1')
             {
                 decimal+=Math.pow(2,power);
@@ -61,21 +70,21 @@ public class StringOperator {
         }
         return decimal;
     }
-public String ExpandString(String input)
-{
-    String ExpandedString="";
-    for(int i=0;i<input.length();i+=2)
+    public String ExpandString(String input)
     {
-        char alphabet=input.charAt(i);//this will store the character
-        char digit=input.charAt(i+1);//this will store the digit
-        int times=digit-'0';//stores how many times the character has to be print
-        for(int j=0;j<times;j++)
+        String ExpandedString="";
+        for(int i=0;i<input.length();i+=2)
         {
-            ExpandedString+=alphabet;
+            char alphabet=input.charAt(i);//this will store the character
+            char digit=input.charAt(i+1);//this will store the digit
+            int times=digit-'0';//stores how many times the character has to be print
+            for(int j=0;j<times;j++)
+            {
+                ExpandedString+=alphabet;
+            }
         }
+        return ExpandedString;
     }
-    return ExpandedString;
-}
     public String CompressString(String input) {
         String compressed = "";
         int count = 1;
@@ -94,73 +103,72 @@ public String ExpandString(String input)
         return compressed;
     }
 
-public boolean PrimeNumberChecker(int number)
-{
-    if (number <= 1) {
-        return false;  //number less than can never be prime
-    }
- //check if the divisibility of number from 2 to square of it
-    for (int i = 2; i * i <= number; i++) {
-        if (number % i == 0) {
-            return false;  // Divisible by i, not a prime number
+    public boolean PrimeNumberChecker(int number)
+    {
+        if (number <= 1) {
+            return false;  //number less than can never be prime
         }
-    }
-    return true;  // if no divisor found it is a prime number
-}
-public String NumberToWords(int number)
-{
-
-    if (number == 0) {
-        return "zero";
-    }
-
-    String[] ones = {
-            "", "one", "two", "three", "four", "five",
-            "six", "seven", "eight", "nine", "ten",
-            "eleven", "twelve", "thirteen", "fourteen",
-            "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
-    };
-
-    String[] tens = {
-            "", "", "twenty", "thirty", "forty",
-            "fifty", "sixty", "seventy", "eighty", "ninety"
-    };
-
-    String words = "";
-    boolean space = false; // to control spacing manually
-
-    if (number / 1000 > 0) {
-        words = words + ones[number / 1000] + " thousand";
-        space = true;
-        number = number % 1000;
-    }
-
-    if (number / 100 > 0) {
-        if (space) {
-            words = words + " ";
-        }
-        words = words + ones[number / 100] + " hundred";
-        space = true;
-        number = number % 100;
-    }
-
-    if (number > 0) {
-        if (space) {
-            words = words + " ";
-        }
-
-        if (number < 20) {
-            words = words + ones[number];
-        } else {
-            words = words + tens[number / 10];
-            if (number % 10 > 0) {
-                words = words + " " + ones[number % 10];
+        //check if the divisibility of number from 2 to square of it
+        for (int i = 2; i * i <= number; i++) {
+            if (number % i == 0) {
+                return false;  // Divisible by i, not a prime number
             }
         }
+        return true;  // if no divisor found it is a prime number
     }
-    return words;
-}
+    public String NumberToWords(int number)
+    {
 
+        if (number == 0) {
+            return "zero";
+        }
+
+        String[] ones = {
+                "", "one", "two", "three", "four", "five",
+                "six", "seven", "eight", "nine", "ten",
+                "eleven", "twelve", "thirteen", "fourteen",
+                "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
+        };
+
+        String[] tens = {
+                "", "", "twenty", "thirty", "forty",
+                "fifty", "sixty", "seventy", "eighty", "ninety"
+        };
+
+        String words = "";
+        boolean space = false; // to control spacing manually
+
+        if (number / 1000 > 0) {
+            words = words + ones[number / 1000] + " thousand";
+            space = true;
+            number = number % 1000;
+        }
+
+        if (number / 100 > 0) {
+            if (space) {
+                words = words + " ";
+            }
+            words = words + ones[number / 100] + " hundred";
+            space = true;
+            number = number % 100;
+        }
+
+        if (number > 0) {
+            if (space) {
+                words = words + " ";
+            }
+
+            if (number < 20) {
+                words = words + ones[number];
+            } else {
+                words = words + tens[number / 10];
+                if (number % 10 > 0) {
+                    words = words + " " + ones[number % 10];
+                }
+            }
+        }
+        return words;
+    }
 
     public int LongestUniqueSubstring(String s) {
         int[] lastIndex = new int[256]; // Store last index of each character (ASCII)
@@ -192,71 +200,70 @@ public String NumberToWords(int number)
         return maxLength;
     }
 
+    private int uniqueCount = 0; // make it class-level so it persists
+    private char[][] unique;     // store unique palindromes
+    public int countUniquePalindromes(String s) {
+        int n = s.length();
+        int count = 0;
+        unique = new char[n * n][n]; // worst-case: n*(n+1)/2 substrings
 
-        public int countUniquePalindromes(String s) {
-            int n = s.length();
-            int count = 0;
+        for (int center = 0; center < n; center++) {
+            // Odd-length palindromes
+            count = expandAndStore(s, center, center, count);
 
-            // 2D array to store unique palindromes
-            char[][] unique = new char[n * n][n]; // worst-case: n*(n+1)/2 substrings
-            int uniqueCount = 0;
-
-            for (int center = 0; center < n; center++) {
-                // Odd-length palindromes
-                count = expandAndStore(s, center, center, unique, uniqueCount, count);
-
-                // Even-length palindromes
-                count = expandAndStore(s, center, center + 1, unique, uniqueCount, count);
-            }
-
-            return count;
+            // Even-length palindromes
+            count = expandAndStore(s, center, center + 1, count);
         }
 
-        // Expand around center and store/check unique palindromes
-        private int expandAndStore(String s, int left, int right, char[][] unique, int uniqueCount, int totalCount) {
-            int n = s.length();
-
-            while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
-                int len = right - left + 1;
-
-                if (len >= 3) { // Only count palindromes of length >= 3
-                    boolean isUnique = true;
-
-                    // Check manually against previously stored palindromes
-                    for (int i = 0; i < uniqueCount; i++) {
-                        boolean same = true;
-                        for (int j = 0; j < len; j++) {
-                            if (unique[i][j] != s.charAt(left + j)) {
-                                same = false;
-                                break;
-                            }
-                        }
-                        if (same) {
-                            isUnique = false;
+        return count;
+    }
+    // Expand around center and store unique palindromes
+    private int expandAndStore(String s, int left, int right, int totalCount) {
+        int n = s.length();
+        while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+            int len = right - left + 1;
+            if (len >= 3) { // Only consider palindromes of length >= 3
+                boolean isUnique = true;
+                // Check if this palindrome already exists in our 'unique' array
+                for (int i = 0; i < uniqueCount; i++) {
+                    boolean same = true;
+                    for (int j = 0; j < len; j++) {
+                        if (unique[i][j] != s.charAt(left + j)) {
+                            same = false;
                             break;
                         }
                     }
 
-                    // Store if unique
-                    if (isUnique) {
-                        for (int j = 0; j < len; j++) {
-                            unique[uniqueCount][j] = s.charAt(left + j);
-                        }
-                        uniqueCount++;
-                        totalCount++;
+                    if (same && unique[i][len] == 0) { // if exact length match found
+                        isUnique = false;
+                        break;
                     }
                 }
 
-                left--;
-                right++;
+                // Store if unique
+                if (isUnique) {
+                    for (int j = 0; j < len; j++) {
+                        unique[uniqueCount][j] = s.charAt(left + j);
+                    }
+                    uniqueCount++;
+                    totalCount++;
+                }
             }
 
-            return totalCount;
+            left--;
+            right++;
         }
 
-
-
+        return totalCount;
     }
+
+
+}
+
+
+
+
+
 
 
 
